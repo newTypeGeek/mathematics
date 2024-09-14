5a32c3dfc6ed0c1002a74826a221010dadc815c7
BPHT <bennyphtam@gmail.com>
2024-09-14 23:25:15 +0800

    Additve modulo group (#1)
    * Add dependency
    
    * Create additive module group
    
    * Remove loguru & config black
    
    * Fix bug for checking inverse
    
    * Add pytest
    
    * Convert to module
    
    * Fix bug in closure
    
    * Rename method
    
    * Add unittests
    
    * Fix typo

---
+++ src/group_theory/additive_modulo_group.py
@@ -0,0 +1,48 @@
+import copy
+import itertools
+
+
+class AdditiveModuloGroup:
+    def __init__(self, order: int) -> None:
+        assert order > 0, "Order must be greater than 0"
+        self._order = order
+        self._elements = set(range(self._order))
+        self._identity = 0
+
+    def is_subgroup(self, elements: set[int]) -> bool:
+        assert len(elements) > 0, f"Input {elements} must not be empty set"
+        return (
+            self._has_identity(elements)
+            and self._is_subset(elements)
+            and self._is_closure(elements)
+            and self._all_has_inverse(elements)
+        )
+
+    def _is_closure(self, elements: set[int]) -> bool:
+        for x, y in itertools.combinations_with_replacement(elements, 2):
+            if (x + y) % self._order not in elements:
+                return False
+        return True
+
+    def _is_subset(self, elements: set[int]) -> bool:
+        return elements.issubset(self._elements)
+
+    def _has_identity(self, elements: set[int]) -> bool:
+        return self._identity in elements
+
+    def _all_has_inverse(self, elements: set[int]) -> bool:
+        # identity is its own inverse, no checking is required
+        elements_ = copy.deepcopy(elements)
+        elements_.remove(self._identity)
+
+        element_has_inverse = {i: False for i in elements_}
+
+        for x, y in itertools.combinations(elements_, 2):
+            if self._is_inverse(x, y):
+                element_has_inverse[x] = True
+                element_has_inverse[y] = True
+
+        return all(element_has_inverse.values())
+
+    def _is_inverse(self, x: int, y: int) -> bool:
+        return (x + y) % self._order == self._identity
---
+++ src/tests/group_theory/test_additive_modulo_group.py
@@ -0,0 +1,168 @@
+import pytest
+
+from group_theory.additive_modulo_group import AdditiveModuloGroup
+
+
+@pytest.fixture
+def identity_sub_group_candidate() -> set[int]:
+    return {0}
+
+
+@pytest.fixture
+def group_order_seven() -> AdditiveModuloGroup:
+    return AdditiveModuloGroup(7)
+
+
+@pytest.fixture
+def seven_order_group_elements() -> set[int]:
+    return {0, 1, 2, 3, 4, 5, 6}
+
+
+@pytest.fixture
+def seven_order_sub_group_candidates() -> list[set[int]]:
+    return [
+        {0},
+        {1},
+        {2},
+        {3},
+        {4},
+        {5},
+        {6},
+        {0, 1},
+        {0, 2},
+        {0, 3},
+        {0, 4},
+        {0, 5},
+        {0, 6},
+        {1, 2},
+        {1, 3},
+        {1, 4},
+        {1, 5},
+        {1, 6},
+        {2, 3},
+        {2, 4},
+        {2, 5},
+        {2, 6},
+        {3, 4},
+        {3, 5},
+        {3, 6},
+        {4, 5},
+        {4, 6},
+        {5, 6},
+        {0, 1, 2},
+        {0, 1, 3},
+        {0, 1, 4},
+        {0, 1, 5},
+        {0, 1, 6},
+        {0, 2, 3},
+        {0, 2, 4},
+        {0, 2, 5},
+        {0, 2, 6},
+        {0, 3, 4},
+        {0, 3, 5},
+        {0, 3, 6},
+        {0, 4, 5},
+        {0, 4, 6},
+        {0, 5, 6},
+        {1, 2, 3},
+        {1, 2, 4},
+        {1, 2, 5},
+        {1, 2, 6},
+        {1, 3, 4},
+        {1, 3, 5},
+        {1, 3, 6},
+        {1, 4, 5},
+        {1, 4, 6},
+        {1, 5, 6},
+        {2, 3, 4},
+        {2, 3, 5},
+        {2, 3, 6},
+        {2, 4, 5},
+        {2, 4, 6},
+        {2, 5, 6},
+        {3, 4, 5},
+        {3, 4, 6},
+        {3, 5, 6},
+        {4, 5, 6},
+        {0, 1, 2, 3},
+        {0, 1, 2, 4},
+        {0, 1, 2, 5},
+        {0, 1, 2, 6},
+        {0, 1, 3, 4},
+        {0, 1, 3, 5},
+        {0, 1, 3, 6},
+        {0, 1, 4, 5},
+        {0, 1, 4, 6},
+        {0, 1, 5, 6},
+        {0, 2, 3, 4},
+        {0, 2, 3, 5},
+        {0, 2, 3, 6},
+        {0, 2, 4, 5},
+        {0, 2, 4, 6},
+        {0, 2, 5, 6},
+        {0, 3, 4, 5},
+        {0, 3, 4, 6},
+        {0, 3, 5, 6},
+        {0, 4, 5, 6},
+        {1, 2, 3, 4},
+        {1, 2, 3, 5},
+        {1, 2, 3, 6},
+        {1, 2, 4, 5},
+        {1, 2, 4, 6},
+        {1, 2, 5, 6},
+        {1, 3, 4, 5},
+        {1, 3, 4, 6},
+        {1, 3, 5, 6},
+        {1, 4, 5, 6},
+        {2, 3, 4, 5},
+        {2, 3, 4, 6},
+        {2, 3, 5, 6},
+        {2, 4, 5, 6},
+        {3, 4, 5, 6},
+        {0, 1, 2, 3, 4},
+        {0, 1, 2, 3, 5},
+        {0, 1, 2, 3, 6},
+        {0, 1, 2, 4, 5},
+        {0, 1, 2, 4, 6},
+        {0, 1, 2, 5, 6},
+        {0, 1, 3, 4, 5},
+        {0, 1, 3, 4, 6},
+        {0, 1, 3, 5, 6},
+        {0, 1, 4, 5, 6},
+        {0, 2, 3, 4, 5},
+        {0, 2, 3, 4, 6},
+        {0, 2, 3, 5, 6},
+        {0, 2, 4, 5, 6},
+        {0, 3, 4, 5, 6},
+        {1, 2, 3, 4, 5},
+        {1, 2, 3, 4, 6},
+        {1, 2, 3, 5, 6},
+        {1, 2, 4, 5, 6},
+        {1, 3, 4, 5, 6},
+        {2, 3, 4, 5, 6},
+        {0, 1, 2, 3, 4, 5},
+        {0, 1, 2, 3, 4, 6},
+        {0, 1, 2, 3, 5, 6},
+        {0, 1, 2, 4, 5, 6},
+        {0, 1, 3, 4, 5, 6},
+        {0, 2, 3, 4, 5, 6},
+        {1, 2, 3, 4, 5, 6},
+        {0, 1, 2, 3, 4, 5, 6},
+    ]
+
+
+def test_itself_is_subgroup(group_order_seven, seven_order_group_elements):
+    assert group_order_seven.is_subgroup(seven_order_group_elements)
+
+
+def test_identity_is_subgroup(group_order_seven, identity_sub_group_candidate):
+    assert group_order_seven.is_subgroup(identity_sub_group_candidate)
+
+
+def test_only_two_candidates_are_subgroups(group_order_seven, seven_order_sub_group_candidates):
+    subgroup_count = 0
+    for candidate in seven_order_sub_group_candidates:
+        subgroup_count += group_order_seven.is_subgroup(candidate)
+
+    # this is due to Lagrange's Theorem
+    assert subgroup_count == 2
