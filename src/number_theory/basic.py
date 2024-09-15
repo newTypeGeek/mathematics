15945bded5d2fdd3f72a42e5b9042ff82f5740ca
BPHT <bennyphtam@gmail.com>
2024-09-15 17:15:31 +0800

    Gcd and lcm (#3)
    * Create number theory module
    
    * Impl. gcd
    
    * Update docstring
    
    * Add unit tests for gcd
    
    * Impl. lcm
    
    * Fix return type of lcm
    
    * Add unit tests for lcm

---
+++ src/number_theory/basic.py
@@ -0,0 +1,15 @@
+def gcd(a: int, b: int) -> int:
+    """
+    Greatest Common Divisor of two integers a and b
+    Here, Euclidean algorithm is used
+    """
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def lcm(a: int, b: int) -> int:
+    """
+    Least Common Multiple of two integers a and b
+    """
+    return (a * b) // gcd(a, b)
