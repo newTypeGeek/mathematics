e43a9f836527e9a97d87bad48e7c677293de595b
bpht <bennyphtam@gmail.com>
2024-09-15 17:08:13 +0800

    Impl. lcm

--- src/number_theory/basic.py
+++ src/number_theory/basic.py
@@ -6,3 +6,10 @@ def gcd(a: int, b: int) -> int:
     if b == 0:
         return a
     return gcd(b, a % b)
+
+
+def lcm(a: int, b: int) -> int:
+    """
+    Least Common Multiple of two integers a and b
+    """
+    return (a * b) / gcd(a, b)
