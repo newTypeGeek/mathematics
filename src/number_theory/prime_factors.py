e1740d410b106ad18742be63294a24e33b4960e7
bpht <bennyphtam@gmail.com>
2024-09-17 18:43:31 +0800

    Add more comments

--- src/number_theory/prime_factors.py
+++ src/number_theory/prime_factors.py
@@ -6,6 +6,8 @@ def brute_force_prime_factors(n: int) -> list[int]:
 
         # if i no longer divisble by n, then we iterate to the next i
         # we don't need to check for i being prime, as we start from 2 which is prime
+        # let say composite number 4 is a factor or n, then its previous i = 2 is also a factor of n
+        # so it should already be extracted
         while n % i == 0:
             # divisble means i is a (prime) factor of n
             prime_factors.append(i)
